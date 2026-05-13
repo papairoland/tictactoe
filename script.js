c21288b04b7b89c5b036ab5efdf6a6d4645f1973
Pápai Roland <144841960+papairoland@users.noreply.github.com>
2026-05-13 19:57:19 +0200

    Alapváltozók és a HTML elemek kiválasztása (DOM)

---
+++ script.js
@@ -0,0 +1,9 @@
+var cellak = document.querySelectorAll('.cella');
+var felirat = document.getElementById('allapot-szoveg');
+var eredmenyPanel = document.getElementById('eredmeny-ablak');
+var inditoPanel = document.getElementById('indito-ablak');
+
+var kiJonEppen = "X";
+var tablaAllapota = ["", "", "", "", "", "", "", "", ""];
+var tartAJatek = true;
+var pontok = { X: 0, O: 0 };
